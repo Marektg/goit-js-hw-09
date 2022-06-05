a9486391e3acba98e6ef3291399c95ab66900c30
Marektg <93349057+Marektg@users.noreply.github.com>
2022-06-05 13:48:03 +0000

    Deploying to gh-pages from @ Marektg/goit-js-hw-09@3ff87ef29215bdf7ba9644895c05a6ddf90f1a5c 🚀

---
+++ 01-color-switcher.194ff779.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector('button[data-start=""]'),n=document.querySelector('button[data-stop=""]'),o=document.querySelector("body");function c(){o.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}e.addEventListener("click",(function(){t=setInterval(c,1e3),e.disabled=!0})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.194ff779.js.map

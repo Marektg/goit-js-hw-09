3ff87ef29215bdf7ba9644895c05a6ddf90f1a5c
Marek Kalus <marektg2@gmail.com>
2022-06-05 15:46:54 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBnt = document.querySelector('button[data-start=""]');
+const stopBnt = document.querySelector('button[data-stop=""]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// startBnt.setAttribute("onclick", "timeChangeColor");
+
+
+let timeChangeColor;
+function changeColor() {
+
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+
+}
+
+startBnt.addEventListener("click", () => {
+      timeChangeColor = setInterval(changeColor, 1000);
+    startBnt.disabled = true;
+});
+stopBnt.addEventListener("click", () => { clearInterval(timeChangeColor); startBnt.disabled = false; });
